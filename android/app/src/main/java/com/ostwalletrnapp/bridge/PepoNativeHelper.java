276dda62af45fe674fece813c8fef77337ccba98
sachinchauhan <sachin@pepo.com>
2019-12-09 19:18:05 +0530

    getGroupAndDecimalSeparators in android added

---
+++ android/app/src/main/java/com/ostwalletrnapp/bridge/PepoNativeHelper.java
@@ -0,0 +1,76 @@
+package com.pepo2.bridge;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+import javax.annotation.Nonnull;
+
+public class PepoNativeHelper extends ReactContextBaseJavaModule {
+    private static final String TAG = "Ost-PepoNativeHelper";
+
+    public PepoNativeHelper(@Nonnull ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "PepoNativeHelper";
+    }
+
+//    @ReactMethod
+//    public void numberFromString(@Nonnull String input, @Nonnull Callback successCallback, @NonNull Callback errorCallback) {
+//        // Always get the NumberFormat Instance.
+//        // Locale can change at run time.
+//        NumberFormat nf = NumberFormat.getInstance();
+//        try {
+//            Number convertedNumber = nf.parse(input);
+//            successCallback.invoke( convertedNumber.doubleValue() );
+//        } catch (Throwable e) {
+//            errorCallback.invoke(e.getLocalizedMessage());
+//        }
+//    }
+//
+//    @ReactMethod
+//    public void stringFromNumber(@Nonnull Double input, @Nonnull Callback successCallback, @Nonnull Callback errorCallback) {
+//        // Always get the NumberFormat Instance.
+//        // Locale can change at run time.
+//        NumberFormat nf = NumberFormat.getInstance();
+//        try {
+//            String formattedNumber = nf.format(input);
+//            successCallback.invoke( formattedNumber );
+//        } catch (Throwable e) {
+//            errorCallback.invoke(e.getLocalizedMessage());
+//        }
+//    }
+
+    @ReactMethod
+    public void getGroupAndDecimalSeparators(@Nonnull Callback successCallback, @Nonnull Callback errorCallback) {
+        double input = 1000.1;
+        // Always get the NumberFormat Instance.
+        // Locale can change at run time.
+        NumberFormat nf = NumberFormat.getInstance();
+        try {
+            String formattedNumber = nf.format(input);
+            int decimalSeparatorIndex = formattedNumber.length() - 2;
+
+            String decimalSeparator = formattedNumber.substring(decimalSeparatorIndex, decimalSeparatorIndex + 1);
+            String groupSeparator = formattedNumber.substring(1, 2);
+
+            if (groupSeparator.equals("0")) {
+                groupSeparator = "";
+            }
+
+            successCallback.invoke(groupSeparator, decimalSeparator );
+        } catch (Throwable e) {
+            errorCallback.invoke(e.getLocalizedMessage());
+        }
+
+    }
+
+}
---
+++ android/app/src/main/java/com/ostwalletrnapp/bridge/PepoNativePackage.java
@@ -0,0 +1,35 @@
+package com.pepo2.bridge;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+public class PepoNativePackage implements ReactPackage {
+    @Nonnull
+    @Override
+    public List<NativeModule> createNativeModules(@Nonnull ReactApplicationContext reactContext) {
+        return Arrays.asList(
+                new PepoNativeHelper(reactContext)
+        );
+    }
+
+    // Deprecated from RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+
+    @Nonnull
+    @Override
+    public List<ViewManager> createViewManagers(@Nonnull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
