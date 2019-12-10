7933a7808609f6a7d154b8b6b0acd0426c1d931f
sachinchauhan <sachin@pepo.com>
2019-12-10 17:07:51 +0530

    Intro Screen

--- src/components/IntroScreen/index.js
+++ src/components/IntroScreen/index.js
@@ -1,9 +1,9 @@
 import React, {PureComponent} from 'react';
 import {
-  SafeAreaView,
-  Text,
-  StatusBar,
-  Image
+	SafeAreaView,
+	Text,
+	StatusBar,
+	Image
 } from 'react-native';
 
 import {OstWalletSdk, OstWalletSdkUI} from '@ostdotcom/ost-wallet-sdk-react-native';
@@ -13,44 +13,68 @@ import ostLog from '../../assets/ostLogoBlue.png'
 
 import ost_sdk_theme_config from '../../theme/ostsdk/ost-sdk-theme-config';
 import ost_sdk_content_config from '../../theme/ostsdk/ost-sdk-content-config';
+import CurrentUser from "../../models/CurrentUser";
+import {SwitchActions} from "react-navigation";
+import AppLoader from "../CommonComponent/AppLoader";
 
 class IntroScreen extends PureComponent {
-  static navigationOptions = ({ navigation, navigationOptions }) => {
-    return {
-      header: null,
-      headerBackTitle: null
-    };
-  };
-  constructor(props) {
-    super(props);
-  }
+	static navigationOptions = ({navigation, navigationOptions}) => {
+		return {
+			header: null,
+			headerBackTitle: null
+		};
+	};
 
-  init = async () => {
-    OstWalletSdkUI.setThemeConfig(ost_sdk_theme_config);
-    OstWalletSdkUI.setContentConfig(ost_sdk_content_config);
-  };
+	constructor(props) {
+		super(props);
+		this.state = {modalVisible: false, title: ""};
+	}
 
-  onSdkInitialized = (error, success) => {
+	init = async () => {
+		OstWalletSdkUI.setThemeConfig(ost_sdk_theme_config);
+		OstWalletSdkUI.setContentConfig(ost_sdk_content_config);
+	};
 
-  };
+	onSdkInitialized = (error, success) => {
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.props.navigation.push('LoginScreen', {"navTitle": "Login to your Account", "isSingupView": true})
-    }, 1000)
-  }
+	};
 
-  render() {
-    return(
-      <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <Image style={styles.ostLogo} source={ostLog}/>
-          <Text>Version 1.0.0</Text>
-        </SafeAreaView>
-      </>
-    )
-  }
+	componentDidMount() {
+		if (!CurrentUser.getUserData()) {
+			this.showLoader(true);
+			CurrentUser.initialize()
+				.then((res) => {
+					this.showLoader(false);
+					this.props.navigation.dispatch(SwitchActions.jumpTo({routeName: 'Wallet'}, {navTitle: "Wallet"}));
+				}).catch((err) => {
+				this.showLoader(false);
+				this.props.navigation.push('LoginScreen', {"navTitle": "Login to your Account", "isSingupView": true})
+			});
+		} else {
+			this.showLoader(false);
+			this.props.navigation.dispatch(SwitchActions.jumpTo({routeName: 'Wallet'}, {navTitle: "Wallet"}));
+		}
+	}
+
+	showLoader(show) {
+		this.setState({
+			modalVisible: show,
+			title: "Trying Auto-Login"
+		});
+	}
+
+	render() {
+		return (
+			<>
+				<AppLoader modalVisible={this.state.modalVisible} title={this.state.title}/>
+				<StatusBar barStyle="dark-content"/>
+				<SafeAreaView>
+					<Image style={styles.ostLogo} source={ostLog}/>
+					<Text>Version 1.0.0</Text>
+				</SafeAreaView>
+			</>
+		)
+	}
 }
 
 export default IntroScreen;
