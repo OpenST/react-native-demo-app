1bf9abf3884b83e7e95384e3997d6716091e097d
Aniket <aniket@ost.com>
2019-12-18 16:43:34 +0530

    fix

--- src/theme/ostsdk/ost-sdk-theme-config.js
+++ src/theme/ostsdk/ost-sdk-theme-config.js
@@ -6,83 +6,6 @@ export default {
   //   "asset_name": "header_icon"
   // },
 
-  "h1": {"size": 20,
-    "color": "#3e404b",
-    "system_font_weight": "semi_bold",
-    "alignment": "center"
-  },
-
-  "h2": {"size": 17,
-    "color": "#666666",
-    "system_font_weight": "medium",
-    "alignment": "center"
-  },
-
-  "h3": {"size": 15,
-    "color": "#888888",
-    "system_font_weight": "regular",
-    "alignment": "center"
-  },
-
-  "h4": {"size": 12,
-    "color": "#888888",
-    "system_font_weight": "regular",
-    "alignment": "center"
-  },
-
-  "c1": {"size": 14,
-    "color": "#484848",
-    "system_font_weight": "bold",
-    "alignment": "left"
-  },
-
-  "c2": {"size": 12,
-    "color": "#6F6F6F",
-    "system_font_weight": "regular",
-    "alignment": "left"
-  },
-
-  "b1": {
-    "size": 17,
-    "color": "#ffffff",
-    "background_color": "#FF5566",
-    "system_font_weight": "medium"
-  },
-
-  "b2": {
-    "size": 17,
-    "color": "#FF5566",
-    "background_color": "#ffffff",
-    "system_font_weight": "semi_bold"
-  },
-
-  "b3": {
-    "size": 12,
-    "color": "#ffffff",
-    "background_color": "#FF5566",
-    "system_font_weight": "medium"
-  },
-
-  "b4": {
-    "size": 12,
-    "color": "#FF5566",
-    "background_color": "#ffffff",
-    "system_font_weight": "medium"
-  },
-
-  "edit_text": {
-    "size": 15,
-    "color": "#101010",
-    "background_color": "#ffffff",
-    "alignment": "left",
-    "system_font_weight": "regular",
-    "placeholder": {
-      "size": 15,
-      "color": "#888888",
-      "system_font_weight": "regular"
-    }
-  },
-
   "navigation_bar": {
     "tint_color": colors.brightSky
   },
@@ -93,10 +16,10 @@ export default {
 
   "icons": {
     "close": {
-      "tint_color": "#3e404b"
+      "tint_color": "#ffffff"
     },
     "back":{
-      "tint_color": "#3e404b"
+      "tint_color": "#ffffff"
     }
   },
 
